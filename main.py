fee8682dd136ae6225987a4725abb4860f3ad4eb
MaximTarutin <mtarut@mail.ru>
2023-12-23 16:38:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from sys import argv
+
+if __name__ == '__main__':
+    filename = argv
+
+    try:
+        check_file = os.path.exists(filename[1])
+        if check_file:
+            command: str = 'ldd ' + filename[1]
+            os.system(command)
+        else:
+            exit()
+    except:
+        print("Не указано имя файла или такого файла нет ...")
